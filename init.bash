2744556aefa512543f8428c2da397c01da6a49b1
Leighton Turner <leightonturner@gmail.com>
2013-11-27 12:13:27 +1300

    Local configuration loading

--- init.bash
+++ init.bash
@@ -1,5 +1,12 @@
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+HOST=`hostname -s`;
 
 source $DIR/var.bash
 source $DIR/fn.bash
+
+# Attempt to load a local configuration in ~/.bash/local/<host_name.bash>
+if [ -f "$DIR/local/$HOST.bash" ]; then
+  source "$DIR/local/$HOST.bash";
+fi
+
