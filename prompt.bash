17e8eb749a05b8003859c4a9b02774bb072cf36f
Leighton Turner <leightonturner@gmail.com>
2015-07-12 17:22:45 +0100

    Update git prompt to include root directory information

--- prompt.bash
+++ prompt.bash
@@ -1,11 +1,13 @@
 
 # requires fn.bash:in_git_repo
 function prompt_command {
-  git_info="";
+  git_preamble=":";
   if in_git_repo ; then
-    git_info=" (`git branch | grep \* | sed s/*\ //`)";
+    git_dir=$(basename `git rev-parse --show-toplevel`)
+    git_info=$(git branch | grep \* | sed s/*\ //);
+    git_preamble=" ($git_dir:$git_info)"
   fi
-  PS1=" \$PROMPT_SYMBOL \W\$git_info: ";
+  PS1=" \$PROMPT_SYMBOL \W\$git_preamble ";
 }
 [ -z "$PS1" ] && return
 setenv PROMPT_COMMAND prompt_command
